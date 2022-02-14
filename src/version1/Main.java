0915df39533c6952543ac80934e476344669a3b8
Ricardo Gonzales <rzamarron20@unm.edu>
2022-02-13 21:43:48 -0700

    added player

--- src/version1/Main.java
+++ src/version1/Main.java
@@ -13,15 +13,69 @@ public class Main {
         Boneyard boneyard = new Boneyard();
         board board = new board();
         boneyard.addDominos();
+        Player player = new Player(boneyard);
 
         boolean madeMove = false;
-        boolean shoulBreak = false;
+        boolean shouldBreak = false;
         boolean placedLeft = false;
         boolean firtTurn = true;
 
         StringBuilder offset = new StringBuilder("  ");
         System.out.println("Boneyard has " + boneyard.getBoneyard().size() + " dominoes");
 
+        do {
+            while (!madeMove) {
+                if (shouldBreak) {
+                    break;
+                }
+                System.out.println("Tray:" + player.accessPlayerHand());
+                System.out.println("Human turn");
+                System.out.println("[p] play Domino" + "\n[d] Draw from boneyard" + "\n[q] Quit");
+                String playChoice = scanner.next();
+                switch (playChoice) {
+                    case "p":
+                        System.out.println("Which domino would you like to place");
+                        int dominoIndex = scanner.nextInt();
+                        String choice = "r";
+
+                        if (!firtTurn) {
+                            System.out.println("Left or right? (1/r)");
+                            choice = scanner.next();
+                            if (choice.equals("1")) {
+                                placedLeft = true;
+                            }
+                        }
+                        System.out.println("Flip domino? (y/n)");
+                        String flipChoice = scanner.next();
+
+                        try {
+                            player.placeDomino(board, dominoIndex, flipChoice, choice);
+                            madeMove = true;
+                            break;
+                        } catch (IllegalArgumentException e) {
+                            System.out.println("Invalid move, try again or draw from the boneyard.");
+                            madeMove = false;
+                            break;
+                        }
+                    case "d" :
+                        player.drawBoneyard(boneyard);
+                        System.out.println("GUI.Boneyard has " + boneyard.getBoneyard().size() + " dominoes");
+                        if (boneyard.getBoneyard().isEmpty()) {
+                            shouldBreak = true;
+                        }
+                    case "q":
+                        System.out.println("Quitting game, thanks for playing!");
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid selection, please try again");
+                }
+            }
+                }
+        while(madeMove); {
+            madeMove = false;
+            Domino lastPlaced = board.getRow().get(board.getRow().size() - 1);
+            }
+        }
 
     }
-}
+
---
+++ src/version1/Player.java
@@ -0,0 +1,59 @@
+package version1;
+import java.util.ArrayList;
+public class Player {
+    private Hand hand = new Hand();
+    private int turn =  1;
+
+    public Player(Boneyard boneyard) {
+        hand.populateHand(boneyard);
+    }
+
+    public void placeDomino(board board, int dominoIndex, String flip, String sideChoice) {
+        Domino domino = hand.getHand().get(dominoIndex);
+        if (flip.equals("y")) {
+            domino.flip();
+        }
+        if (turn == 1) {
+            if (sideChoice.equals("1")) {
+                board.addLeftRow(domino);
+            } else {
+                board.addRow(domino);
+                turn++;
+            }
+            hand.getHand().remove(domino);
+        } else {
+            Domino lastPlaced = board.getRow().get(board.getRow().size() - 1);
+            if(sideChoice.equals("1")) {
+                Domino firstDomino = board.getRow().get(0);
+                if(domino.getFlip2() == firstDomino.getFlip1() || domino.getFlip2() == 0 ||
+                        firstDomino.getFlip1() == 0) {
+                    board.addLeftRow(domino);
+                    hand.getHand().remove(domino);
+                } else {
+                    throw new IllegalArgumentException("Invalid move, try again or draw from the boneyard");
+                }
+            } else {
+                if (domino.getFlip1() == lastPlaced.getFlip2() || domino.getFlip2() == 0 || lastPlaced.getFlip2() == 0){
+                    board.addRow(domino);
+                    hand.getHand().remove(domino);
+
+                } else {
+                    throw new IllegalArgumentException("Invalid move, try again, or draw from the boneyard");
+                }
+            }
+        }
+    }
+    public void drawBoneyard(Boneyard boneyard){
+        Domino domino = boneyard.getBoneyard().get(0);
+        boneyard.getBoneyard().remove(domino);
+        hand.getHand().add(domino);
+    }
+
+    public ArrayList<Domino> accessPlayerHand(){
+        return hand.getHand();
+    }
+
+    public Hand getHand(){
+        return hand;
+    }
+}
