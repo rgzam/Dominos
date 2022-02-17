229686e7e55b71d1204def5adc0a080f3f7fc465
Ricardo Gonzales <rzamarron20@unm.edu>
2022-02-16 22:29:37 -0700

    v2?

---
+++ src/Version2/board.java
@@ -0,0 +1,34 @@
+package Version2;
+
+import version1.Domino;
+
+import java.util.ArrayList;
+
+public class board {
+    private ArrayList<Domino> row = new ArrayList<>();
+
+    /**
+     * Adds a domino at the end of the row
+     * @param domino
+     */
+    public void addRow(Domino domino){
+        row.add(domino);
+
+    }
+
+    /**
+     * Adds a domino at the beginning of the row
+     * @param domino
+     */
+    public void addLeftRow (Domino domino){
+        row.add(0, domino);
+    }
+
+    /**
+     * Getter method
+     * @return
+     */
+    public ArrayList<Domino> getRow(){
+        return row;
+    }
+}
